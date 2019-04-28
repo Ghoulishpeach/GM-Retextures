a4c11c89b9cf7a16e878a6e5cf7e6964795b31d5
Ghoulishpeach <50057273+Ghoulishpeach@users.noreply.github.com>
2019-04-27 22:45:54 -0400

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,13 +1,13 @@
 name = "GM Retextures";
-picture = "GMRE_Logo.paa";
+picture = "gmremod.paa";
 actionName = "Website";
 action = "https://github.com/Ghoulishpeach/GM-Retextures";
-logo = "GMRE_Logo.paa";
-logoOver = "GMRE_Logo.paa";
+logo = "gmremod.paa";
+logoOver = "gmremod.paa";
 tooltip = "GM Retextures";
 tooltipOwned = "GM Retextures";
 overview = "A Retexture mod that expands the scope of assets in the Global Mobilization CDLC";
 author = "Peach";
-overviewPicture = "GMRE_Logo.paa";
+overviewPicture = "gmremod.paa";
 overviewText = "GM Retextures";
 overviewFootnote = "";
