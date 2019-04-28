0c272af0259900b50fb2b2930d0901fe068683d7
Ghoulishpeach <50057273+Ghoulishpeach@users.noreply.github.com>
2019-04-28 16:20:33 -0400

    Create cfgWeapons.hpp

---
+++ addons/cfgWeapons.hpp
@@ -0,0 +1,23 @@
+class cfgWeapons
+{
+    class placeholderclass;
+      
+    class placeholderclass: placeholderclass
+    {
+        author = "Peach";
+        scope = 2;
+        displayName = "M/75 Field Dress";
+        picture = "";
+        model = "";
+        hiddenSelections[] = {""};
+        hiddenSelectionsTextures[] = {""};
+         
+        class ItemInfo: placeholderclass
+        {
+            uniformModel = "-";
+            uniformClass = placeholderclass;
+            containerClass = Supply40;
+            mass = 40;
+        };
+    };
+};
