0b63d88117bf431a9ef040d8aa58f882517bf64e
Ghoulishpeach <50057273+Ghoulishpeach@users.noreply.github.com>
2019-04-28 17:51:32 -0400

    Update cfgVehicles.hpp
    populated classnames and standardized indentation for ease of reading

--- addons/cfgVehicles.hpp
+++ addons/cfgVehicles.hpp
@@ -14,27 +14,26 @@
 	#define mag_12(a) a, a, a, a, a, a, a, a, a, a, a, a
 	#define mag_26(a) a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a
 
-	class ;
-    class  :
+	class gm_ge_army_rifleman_g3a3_80_ols;
+    class gmre_nor_m75_base : gm_ge_army_rifleman_g3a3_80_ols
 	{
-		_generalMacro = "";
-		editorPreview = "";
-		side = 0;
-		scope = 1;
-		scopecurator =2;
-		faction = gmre_nor; // Puts unit under new faction
-		vehicleClass = "forsvaret"; // Puts unit in the vehicleclass
-		//model = "\rhsgref\addons\rhsgref_infantry\gear_gue\gue_m93";
-		nakedUniform = "U_BasicBody";
-		uniformClass = "CUP_U_O_TK_Officer";  // Uniform Class
-		backpack = "B_Backpack_Invisible_XLarge"; // Backpack Class
-		linkedItems[]        = {"CUP_V_O_Ins_Carrier_Rig_MG", "rhsgref_fieldcap_ttsko_mountain","ItemMap", "ItemCompass", "ItemWatch",  "ItemRadio"}; // Item's added to the unit.
-		respawnLinkedItems[] = {"CUP_V_O_Ins_Carrier_Rig_MG", "rhsgref_fieldcap_ttsko_mountain","ItemMap", "ItemCompass", "ItemWatch",  "ItemRadio"}; // Item's added to the unit. Should be identical to the linkedItems section.
-		items[]              = {"ACE_packingBandage", "ACE_fieldDressing", "ACE_morphine", "ACE_tourniquet"};
-		respawnItems[]       = {"ACE_packingBandage", "ACE_fieldDressing", "ACE_morphine", "ACE_tourniquet"};
+		_generalMacro 		 	= "gmre_nor_m75_base";
+		editorPreview 		 	= "";
+		side 				 	= 0;
+		scope 				 	= 1;
+		scopecurator 		 	= 2;
+		faction 			 	= gmre_nor; // Puts unit under new faction
+		vehicleClass 		 	= "forsvaret"; // Puts unit in the vehicleclass
+		nakedUniform 			= "U_BasicBody";
+		uniformClass 			= "gmre_nor_m75";  // Uniform Class
+		backpack 			 	= "gm_dk_army_backpack_73_oli"; // Backpack Class
+		linkedItems[]        	= {"gm_ge_army_vest_80_rifleman","gm_ge_headgear_m62_net","ItemMap","gm_ge_army_conat2","gm_watch_kosei_80","ItemRadio"}; // "vest","headgear","map","compass","watch","radio"
+		respawnLinkedItems[] 	= {"gm_ge_army_vest_80_rifleman","gm_ge_headgear_m62_net","ItemMap","gm_ge_army_conat2","gm_watch_kosei_80","ItemRadio"}; // Item's added to the unit. Should be identical to the linkedItems section.
+		items[]              	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//medical supplies
+		respawnItems[]       	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//identical to items[]
 	};
 
-SAMPLE BLANK CLASS
+/*SAMPLE BLANK CLASS
 
 	{
 		_generalMacro = "";
@@ -56,4 +55,4 @@ iconManVirtual = "\A3\ui_f\data\map\vehicleicons\iconManVirtual_ca.paa";
 iconManAT = "\A3\ui_f\data\map\vehicleicons\iconManAT_ca.paa";
 iconManLeader = "\A3\ui_f\data\map\vehicleicons\iconManLeader_ca.paa";
 iconManMG = "\A3\ui_f\data\map\vehicleicons\iconManMG_ca.paa";
-iconManOfficer = "\A3\ui_f\data\map\vehicleicons\iconManOfficer_ca.paa";
+iconManOfficer = "\A3\ui_f\data\map\vehicleicons\iconManOfficer_ca.paa";*/
