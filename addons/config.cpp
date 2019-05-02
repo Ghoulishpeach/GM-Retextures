232d6a02233ce880afb7343a46950fee23f231f1
Ghoulishpeach <50057273+Ghoulishpeach@users.noreply.github.com>
2019-05-01 22:36:25 -0400

    h

--- addons/config.cpp
+++ addons/config.cpp
@@ -1,5 +1,5 @@
 class CfgPatches {
-	class  GMRE{
+	class GMRE{
 		units[] = {
 			"gmre_nor_antitank_g3a3_pz44_80_oli","gmre_nor_antitank_g3a3_pz44_80_ols","gmre_nor_antitank_g3a3_pz44_parka_80_oli","gmre_nor_antitank_g3a3_pz44_parka_80_ols","gmre_nor_antitank_g3a3_pz44_parka_80_win","gmre_nor_antitank_g3a3_pz44_80_m75","gmre_nor_antitank_g3a3_pz84_80_oli","gmre_nor_antitank_g3a3_pz84_80_ols","gmre_nor_antitank_g3a3_pz84_parka_80_oli","gmre_nor_antitank_g3a3_pz84_parka_80_ols","gmre_nor_antitank_g3a3_pz84_parka_80_win","gmre_nor_antitank_g3a3_pz84_80_m75","gmre_nor_antitank_assistant_g3a3_pzf44_80_oli","gmre_nor_antitank_assistant_g3a3_pzf44_80_ols","gmre_nor_antitank_assistant_g3a3_pzf44_parka_80_oli","gmre_nor_antitank_assistant_g3a3_pzf44_parka_80_ols","gmre_nor_antitank_assistant_g3a3_pzf44_parka_80_win","gmre_nor_antitank_assistant_g3a3_pzf84_80_oli","gmre_nor_antitank_assistant_g3a3_pzf84_80_ols","gmre_nor_antitank_assistant_g3a3_pzf84_parka_80_oli","gmre_nor_antitank_assistant_g3a3_pzf84_parka_80_ols","gmre_nor_antitank_assistant_g3a3_pzf84_parka_80_win","gmre_nor_antitank_assistant_g3a3_pzf44_80_m75","gmre_nor_antitank_assistant_g3a3_pzf84_80_m75","gmre_nor_Driver_mp2a1_80_oli ","gmre_nor_demolition_g3a4_80_oli","gmre_nor_demolition_g3a4_80_ols","gmre_nor_demolition_g3a4_parka_80_oli","gmre_nor_demolition_g3a4_parka_80_ols","gmre_nor_demolition_g3a4_parka_80_win","gmre_nor_demolition_g3a4_80_m75","gmre_nor_Engineer_g3a4_80_oli","gmre_nor_Engineer_g3a4_80_ols","gmre_nor_Engineer_g3a4_parka_80_oli","gmre_nor_Engineer_g3a4_parka_80_ols","gmre_nor_Engineer_g3a4_parka_80_win","gmre_nor_Engineer_g3a4_80_m75","gmre_nor_grenadier_g3a3_80_oli","gmre_nor_grenadier_g3a3_80_ols","gmre_nor_grenadier_g3a3_parka_80_oli","gmre_nor_grenadier_g3a3_parka_80_ols","gmre_nor_grenadier_g3a3_parka_80_win","gmre_nor_grenadier_g3a3_80_m75","gmre_nor_machinegunner_mg3_80_oli","gmre_nor_machinegunner_mg3_80_ols","gmre_nor_machinegunner_mg3_parka_80_oli","gmre_nor_machinegunner_mg3_parka_80_ols","gmre_nor_machinegunner_mg3_parka_80_win","gmre_nor_machinegunner_mg3_80_m75","gmre_nor_machinegunner_assistant_g3a3_mg3_80_oli","gmre_nor_machinegunner_assistant_g3a3_mg3_80_ols","gmre_nor_machinegunner_assistant_g3a3_mg3_parka_80_oli","gmre_nor_machinegunner_assistant_g3a3_mg3_parka_80_ols","gmre_nor_machinegunner_assistant_g3a3_mg3_parka_80_win","gmre_nor_machinegunner_assistant_g3a3_mg3_80_m75","gmre_nor_medic_g3a3_80_oli","gmre_nor_medic_g3a3_80_ols","gmre_nor_medic_g3a3_parka_80_oli","gmre_nor_medic_g3a3_parka_80_ols","gmre_nor_medic_g3a3_parka_80_win","gmre_nor_medic_g3a3_80_m75","gmre_nor_militarypolice_p1_80_oli","gmre_nor_militarypolice_p1_80_ols","gmre_nor_militarypolice_p1_parka_80_oli","gmre_nor_militarypolice_p1_parka_80_ols","gmre_nor_militarypolice_p1_parka_80_win","gmre_nor_militarypolice_p1_80_m75","gmre_nor_officer_p1_80_oli","gmre_nor_officer_p1_80_ols","gmre_nor_officer_p1_parka_80_oli","gmre_nor_officer_p1_parka_80_ols","gmre_nor_officer_p1_parka_80_win","gmre_nor_officer_p1_80_m75","gmre_nor_platoonleader_g3a3_p2a1_80_oli","gmre_nor_platoonleader_g3a3_p2a1_80_ols","gmre_nor_platoonleader_g3a3_p2a1_parka_80_oli","gmre_nor_platoonleader_g3a3_p2a1_parka_80_ols","gmre_nor_platoonleader_g3a3_p2a1_parka_80_win","gmre_nor_platoonleader_g3a3_p2a1_80_m75","gmre_nor_rifleman_g3a3_80_oli","gmre_nor_rifleman_g3a3_80_ols","gmre_nor_rifleman_g3a3_parka_80_oli","gmre_nor_rifleman_g3a3_parka_80_ols","gmre_nor_rifleman_g3a3_parka_80_win","gmre_nor_rifleman_g3a3_80_m75","gmre_nor_squadleader_g3a3_p2a1_80_oli","gmre_nor_squadleader_g3a3_p2a1_80_ols","gmre_nor_squadleader_g3a3_p2a1_parka_80_oli","gmre_nor_squadleader_g3a3_p2a1_parka_80_ols","gmre_nor_squadleader_g3a3_p2a1_parka_80_win","gmre_nor_squadleader_g3a3_p2a1_80_m75","grme_nor_crew_mp2a1_80_tank","grme_nor_crew_mp2a1_80_pilot"
 		};
@@ -28,9 +28,171 @@ class CfgVehicleClasses
 		side = 0;
 	};
 };
+class cfgWeapons
+{
+        class Uniform_Base;
+	class UniformItem;
+        class gmre_nor_m75 : Uniform_Base
+        {
+                author= "Peach";
+                scope = 2;
+                displayName = "M/75 Field Dress";
+                picture = "";
+                model = "";
+                hiddenSelections[] = {"M/75 Pattern"};
+                hiddenSelectionsTextures[] = {"GMRE\GM-Retextures\addons\Data\gmre_nor_m75.paa"};
+                class ItemInfo : uniformItem
+                	{
+                        	uniformModel = "-";
+                        	uniformClass = gmre_nor_rifleman_g3a3_80_m75;
+                        	containerClass = Supply40;
+                        	mass = 40;
+                	};
+       	};
+};
 class CfgVehicles
 {
-	#include "cfgVehicles.hpp"
+
+	/// Magazines macros definition ///
+
+	#define mag_2(a) a, a
+	#define mag_3(a) a, a, a
+	#define mag_4(a) a, a, a, a
+	#define mag_5(a) a, a, a, a, a
+	#define mag_6(a) a, a, a, a, a, a
+	#define mag_7(a) a, a, a, a, a, a, a
+	#define mag_8(a) a, a, a, a, a, a, a, a
+	#define mag_9(a) a, a, a, a, a, a, a, a, a
+	#define mag_10(a) a, a, a, a, a, a, a, a, a, a
+	#define mag_11(a) a, a, a, a, a, a, a, a, a, a, a
+	#define mag_12(a) a, a, a, a, a, a, a, a, a, a, a, a
+	#define mag_26(a) a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a
+
+	class gm_ge_army_rifleman_80_oli;
+	class gm_ge_army_rifleman_80_ols;
+	class gm_ge_army_rifleman_parka_80_oli;
+	class gm_ge_army_rifleman_parka_80_ols;
+	class gm_ge_army_rifleman_parka_80_win;
+	class gm_ge_army_crew_80_oli;
+	class gmre_nor_m75_base : gm_ge_army_rifleman_80_ols
+		{
+			_generalMacro 		= "gmre_nor_m75_base";
+			editorPreview 		= "";
+			side 			= 0;
+			scope 			= 1;
+			scopecurator 		= 2;
+			faction 		= gmre_nor; // Puts unit under new faction
+			vehicleClass 		= "forsvaret"; // Puts unit in the vehicleclass
+			nakedUniform 		= "U_BasicBody";
+			uniformClass 		= "gmre_nor_m75";  // Uniform Class
+			backpack 		= "gm_dk_army_backpack_73_oli"; // Backpack Class
+			items[]              	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//medical supplies
+			respawnItems[]       	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//identical to items[]
+		};
+	class gmre_nor_ols_base : gm_ge_army_rifleman_80_ols
+		{
+			_generalMacro 		= "gmre_nor_ols_base";
+			editorPreview 		= "";
+			side 			= 0;
+			scope 			= 1;
+			scopecurator 		= 2;
+			faction 		= gmre_nor; // Puts unit under new faction
+			vehicleClass 		= "forsvaret"; // Puts unit in the vehicleclass
+			nakedUniform 		= "U_BasicBody";
+			uniformClass 		= "gm_ge_army_uniform_soldier_80_ols";  // Uniform Class
+			backpack 		= "gm_dk_army_backpack_73_oli"; // Backpack Class
+			items[]              	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//medical supplies
+			respawnItems[]       	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//identical to items[]
+		};
+	class gmre_nor_oli_base : gm_ge_army_rifleman_80_oli
+		{
+			_generalMacro 		= "gmre_nor_oli_base";
+			editorPreview 		= "";
+			side 			= 0;
+			scope 			= 1;
+			scopecurator 		= 2;
+			faction 		= gmre_nor; // Puts unit under new faction
+			vehicleClass 		= "forsvaret"; // Puts unit in the vehicleclass
+			nakedUniform 		= "U_BasicBody";
+			uniformClass 		= "gm_ge_army_uniform_soldier_80_oli";  // Uniform Class
+			backpack 		= "gm_dk_army_backpack_73_oli"; // Backpack Class
+			items[]              	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//medical supplies
+			respawnItems[]       	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//identical to items[]
+		};
+	class gmre_nor_parka_oli_base : gm_ge_army_rifleman_parka_80_oli
+		{
+			_generalMacro 		= "gmre_nor_parka_oli_base";
+			editorPreview 		= "";
+			side 			= 0;
+			scope 			= 1;
+			scopecurator 		= 2;
+			faction 		= gmre_nor; // Puts unit under new faction
+			vehicleClass 		= "forsvaret"; // Puts unit in the vehicleclass
+			nakedUniform 		= "U_BasicBody";
+			uniformClass 		= "gm_ge_army_uniform_soldier_parka_80_oli";  // Uniform Class
+			backpack 		= "gm_dk_army_backpack_73_oli"; // Backpack Class
+			items[]              	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//medical supplies
+			respawnItems[]       	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//identical to items[]
+		};
+	class gmre_nor_parka_ols_base : gm_ge_army_rifleman_parka_80_ols
+		{
+			_generalMacro 		= "gmre_nor_parka_ols_base";
+			editorPreview 		= "";
+			side 			= 0;
+			scope 			= 1;
+			scopecurator 		= 2;
+			faction 		= gmre_nor; // Puts unit under new faction
+			vehicleClass 		= "forsvaret"; // Puts unit in the vehicleclass
+			nakedUniform 		= "U_BasicBody";
+			uniformClass 		= "gm_ge_army_uniform_soldier_parka_80_ols";  // Uniform Class
+			backpack 		= "gm_dk_army_backpack_73_oli"; // Backpack Class
+			items[]              	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//medical supplies
+			respawnItems[]       	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//identical to items[]
+		};
+	class gmre_nor_parka_win_base : gm_ge_army_rifleman_parka_80_win
+		{
+			_generalMacro 		= "gmre_nor_parka_win_base";
+			editorPreview 		= "";
+			side 			= 0;
+			scope 			= 1;
+			scopecurator 		= 2;
+			faction 		= gmre_nor; // Puts unit under new faction
+			vehicleClass 		= "forsvaret"; // Puts unit in the vehicleclass
+			nakedUniform 		= "U_BasicBody";
+			uniformClass 		= "gm_dk_army_uniform_soldier_84_win";  // Uniform Class
+			backpack 		= "gm_dk_army_backpack_73_oli"; // Backpack Class
+			items[]              	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//medical supplies
+			respawnItems[]       	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};//identical to items[]
+		};
+	class gmre_nor_crew_base : gm_ge_army_crew_80_oli
+		{
+			_generalMacro 		= "gmre_nor_crew_base";
+			editorPreview 		= "";
+			side 			= 0;
+			scope 			= 1;
+			scopecurator 		= 2;
+			faction 		= gmre_nor; // Puts unit under new faction
+			vehicleClass 		= "forsvaret"; // Puts unit in the vehicleclass
+			nakedUniform 		= "U_BasicBody";
+			uniformClass 		= "gm_ge_army_uniform_crew_80_oli";  // Uniform Class
+			backpack 		= "gm_ge_backpack_satchel_80_blk"; // Backpack Class
+			items[]              	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};
+			respawnItems[]       	= {"gm_ge_army_gauzeBandage", "gm_ge_army_gauzeCompress"};
+		};
+	#include "cfgAnti_Tank.hpp"
+	#include "cfgAnti_Tank_Assistant.hpp"
+	#include "cfgCrew.hpp"
+	#include "cfgDemolition.hpp"
+	#include "cfgEngineer.hpp"
+	//#include "cfgGrenadier.hpp"
+	#include "cfgMachine_Gunner.hpp"
+	#include "cfgMachine_Gunner_Assistant.hpp"
+	#include "cfgMedic.hpp"
+	//#include "cfgMilitary_Police.hpp"
+	#include "cfgOfficer.hpp"
+	#include "cfgPlatoon_Leader.hpp"
+	#include "cfgRifleman.hpp"
+	#include "cfgSquad_Leader.hpp"
 };
 class cfgGroups
 {
